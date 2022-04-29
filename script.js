ec3177ec0db108e84d16717523ae091f236a2966
Katie Schultz <ks.katieschultz@gmail.com>
2022-04-29 17:23:49 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function getAreaOfCircle(radius) {
+  return Math.PI * (radius * radius);
+}
+
+console.log(getAreaOfCircle(4));
+
+const getCircumferenceOfCircle = function (radius) {
+  return radius * 2 * Math.PI;
+};
+
+console.log(getCircumferenceOfCircle(4));
+
+function getAreaOfSquare(side) {
+  return side * side;
+}
+
+console.log(getAreaOfSquare(5));
+
+const getAreaOfTriangle = function (base, height) {};
